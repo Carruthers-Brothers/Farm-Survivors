388692e633063db095bb0c645d3c560922846ff1
Cameron Carruthers <cameronlcarruthers@gmail.com>
2024-08-12 18:52:25 -0600

    feat: Add projectile base class

--- Scripts/pine_cone.gd
+++ Scripts/pine_cone.gd
@@ -1,6 +1,11 @@
 extends "res://Scripts/projectile.gd"
 
+var direction = Vector2(randf_range(-1, 1), randf_range(-1, 1))
+
 func _ready():
 	distance_to_travel = randi_range(25, 100)
-	direction = Vector2(randf_range(-1, 1), randf_range(-1, 1))
+
+func get_direction():
+	return direction
+
 
--- Scripts/projectile.gd
+++ Scripts/projectile.gd
@@ -4,14 +4,20 @@ var speed = 300
 var damage = 5
 var distance_to_travel
 var distance_traveled = 0
-var direction
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
+	print(distance_to_travel)
 	if distance_traveled > distance_to_travel:
 		return
+		#reach_destination()
+		#return
 	
-	position += direction * speed * delta # not a character body, so doesn't have move_and_slide(), need delta
+	var direction = get_direction()
+	
+	if direction != null:
+		position += direction * speed * delta # not a character body, so doesn't have move_and_slide(), need delta
+		
 	distance_traveled += speed * delta
 
 
@@ -20,3 +26,9 @@ func _on_area_entered(area):
 	if enemy.has_method("take_damage"):
 		enemy.call_deferred("take_damage", damage)
 		queue_free()
+
+func reach_destination():
+	return
+
+func get_direction():
+	return null
